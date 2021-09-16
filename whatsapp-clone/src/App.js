9b0415cecc08396bf99457bc36bc8b7ef9cdc52e
stefanobergamini <sbpoletto@hotmail.com>
2021-09-16 15:32:00 -0300

    Start of project material-ui installed and working

---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App_body">
+        <Sidebar />
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUQYP1fzji-qcOOGS_bltL4dDrj9R3-ns",
+    authDomain: "whatsapp-clone-cf2f3.firebaseapp.com",
+    projectId: "whatsapp-clone-cf2f3",
+    storageBucket: "whatsapp-clone-cf2f3.appspot.com",
+    messagingSenderId: "743261708198",
+    appId: "1:743261708198:web:1f50f29dfe147646e47967"
+};

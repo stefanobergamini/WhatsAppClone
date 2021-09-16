212300efe55a181fb4faf447d9919dd6fee964e6
stefanobergamini <sbpoletto@hotmail.com>
2021-09-16 16:43:35 -0300

    Left bar with widgets and search bar

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import './Sidebar.css'
-import { Avatar } from "@material-ui/core"
+import { Avatar, IconButton } from "@material-ui/core"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import { SearchOutlined } from '@material-ui/icons'
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
                 <Avatar />
+                <div className="sidebar_headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
 
             <div className="sidebar_search">
-
+                <div className="sidebar_container">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
             </div>
             
             <div className="sidebar_chats">
